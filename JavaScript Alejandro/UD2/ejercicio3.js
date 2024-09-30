364d63d26cea9d9acd9f763885d534b8e674347c
FranPcClase <fran.5oct99@gmail.com>
2024-09-30 15:05:55 +0200

    a

--- JavaScript Alejandro/UD2/ejercicio3.js
+++ JavaScript Alejandro/UD2/ejercicio3.js
@@ -1,28 +1,9 @@
-function dibujarRectangulo(n) {
-    let rectangulo = [];
+let resultado=[]; //Cuadrado
 
-    // Validar que el número sea mayor o igual a 2 para poder dibujar un rectángulo hueco
-    if (n < 2) {
-        return ["El número debe ser mayor o igual a 2"];
+function pintaCuadrado(n){
+    for(let f=0; f<n; f++){ //filas
+        for(let c=0; c<n; c++){ //columnas
+
+        }
     }
-
-    // Primera fila (completamente llena)
-    rectangulo.push('*'.repeat(n));
-
-    // Filas intermedias (huecas)
-    for (let i = 0; i < n - 2; i++) {
-        rectangulo.push('*' + ' '.repeat(n - 2) + '*');
-    }
-
-    // Última fila (completamente llena, igual que la primera)
-    rectangulo.push('*'.repeat(n));
-
-    return rectangulo;
 }
-
-// Código auxiliar para probar la función
-let numero = 5; // Puedes cambiar este número para probar con otros valores
-let rectanguloHueco = dibujarRectangulo(numero);
-
-// Mostrar el rectángulo en la consola
-rectanguloHueco.forEach(linea => console.log(linea));
