ba5cc27e325e1dc7b2ceba9f742a639ce4f9a0ca
franrollo99 <fran.5oct99@gmail.com>
2024-10-03 13:12:04 +0200

    a

--- PHP Carmen/UD3/Hoja03_PHP_04.php
+++ PHP Carmen/UD3/Hoja03_PHP_04.php
@@ -24,14 +24,42 @@
     
     <h3>Ejercicio 2</h3>
     <?php
+        $coche=new Coche("7479FLT", 90);
+
+        $coche->mostrarInfo();
         
+        $coche->acelera(20);
+        echo "<br>El coche ha acelerado a " . $coche->getVelocidad() . "km/h <br>";
+
+        $coche->frena(100);
+        echo "El coche ha frenado hasta los " . $coche->getVelocidad() . "km/h <br>";
+
+        //Freno mas alla del limite
+        $coche->frena(120);
+        echo "El coche ha frenado hasta los " . $coche->getVelocidad() . "km/h. Se ha detenido";
+
+
     ?>
 
     <br><br><br>
     
     <h3>Ejercicio 3</h3>
     <?php
+        $monederoFran=new Monedero();
 
+        $monederoFran->meterDinero(10000);
+        echo "Fran tiene en el monedero:" . $monederoFran->mostrarDinero() . "€ <br>";
+
+        $monederoFran->sacarDinero(3000);
+        echo "Fran tiene en el monedero:" . $monederoFran->mostrarDinero() . "€ <br>";
+
+        $monederoPaco=new Monedero(); //Creamos un nuevo monedero
+
+        echo "Hay un total de " . Monedero::cantidadMonederos() . " monederos <br>"; //Llamamos a la funcion estatica cantidadMonederos()
+
+        unset($monederoPaco); //Destruimos un monedero
+
+        echo "Hay un total de " . Monedero::cantidadMonederos() . " monederos";
     ?>
 
     <br><br><br>
--- PHP Carmen/UD3/Hoja03_PHP_04Funciones.php
+++ PHP Carmen/UD3/Hoja03_PHP_04Funciones.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<h3>Ejercicio 1</h3>
+    <!-- <h3>Ejercicio 1</h3> -->
     <?php
         class Circulo{
             private $radio;
@@ -21,7 +21,7 @@
                 $this->radio=$radio;
             }
 
-            //gerRadio
+            //getRadio
             public function getRadio(): float{
                 return $this->radio;
             }
@@ -39,7 +39,8 @@
     ?>
 
 
-<h3>Ejercicio 2</h3>
+
+    <!-- <h3>Ejercicio 2</h3> -->
     <?php
         class Coche{
             private $matricula;
@@ -50,10 +51,94 @@
                 $this->velocidad=$velocidad;
             }
 
-            public function setMatricula(string $matricula){
+            public function setMatricula(string $matricula): void{
                 $this->matricula=$matricula;
+            }
+
+            public function getmatricula(): string{
+                return $this->matricula;
+            }
+
+            public function setVelocidad(int $velocidad): void{
+                $this->velocidad=$velocidad;
+            }
+
+            public function getVelocidad(): int{
+                return $this->velocidad;
+            }
+
+            public function acelera(int $incremento): void{
+                $nuevaVelocidad=$this->velocidad+$incremento;
+
+                if($nuevaVelocidad>120){
+                    $this->velocidad=120;
+                }else{
+                    $this->velocidad=$nuevaVelocidad;
+                }
+            }
+
+            public function frena(int $decremento): void{
+                $nuevaVelocidad=$this->velocidad-$decremento;
+
+                if($nuevaVelocidad<0){
+                    $this->velocidad=0;
+                }else{
+                    $this->velocidad=$nuevaVelocidad;
+                }
+            }
+
+            public function mostrarInfo(): void{
+                echo "Matricula " . $this->getMatricula() . "<br>";
+                echo "Velocidad " . $this->getVelocidad() . "km/h";
             }
         }
     ?>
+
+
+
+    <!-- <h3>Ejercicio 3</h3> -->
+    <?php
+        class Monedero{
+            private $dinero; //Euros, sin centimos, sino seria float
+            private static $numeroMonederos=0;
+
+            public function __construct(int $dineroInicial=0){
+                $this->dinero=$dineroInicial;
+                self::$numeroMonederos++;
+            }
+
+            public function __destruct()
+            {
+                self::$numeroMonederos--; //Decrementa el numero de monederos cuando se 
+            }
+
+            public function meterDinero(int $cantidad):void{
+                if($cantidad>0){
+                    $this->dinero+=$cantidad;
+                }else{
+                    echo "La cantidad debe ser mayor que 0";
+                }
+            }
+
+            public function sacarDinero(int $cantidad):void{
+                if($cantidad>0 || $cantidad<$this->dinero){
+                    $this->dinero-=$cantidad;
+                }else if($cantidad>$this->dinero){
+                    echo "Cantidad no valida para sacar, puedes sacar " . $this->dinero . "€ o menos";
+                }else{
+                    echo "La cantidad debe ser mayor que 0";
+                }
+            }
+
+            public function mostrarDinero():int{
+                return $this->dinero;
+            }
+
+            public static function cantidadMonederos():int{
+                return self::$numeroMonederos;
+            }
+        }
+    ?>
+
 </body>
 </html>
