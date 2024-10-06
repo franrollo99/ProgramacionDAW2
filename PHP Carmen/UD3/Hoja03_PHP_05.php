e7f7dfad48ab8dbdc09978afea2b1ee2607d4dc9
franrollo99 <fran.5oct99@gmail.com>
2024-10-07 01:46:09 +0200

    a

--- PHP Carmen/UD3/Hoja03_PHP_05.php
+++ PHP Carmen/UD3/Hoja03_PHP_05.php
@@ -40,13 +40,76 @@
 
     <h3>Ejercicio 3</h3>
     <?php
+        $medicos=[
+            new Familia("Fran", 25, "tarde", 5),
+            new Familia("Maria", 65, "tarde", 19),
+            new Familia("Mario", 50, "mañana", 3),
+            new Urgencia("Pepe", 68, "tarde", "cardiologia"),
+            new Urgencia("Tomas", 59, "tarde", "traumatologia"),
+            new Urgencia("Lucia", 61, "mañana", "cardiologia"),
+            new Urgencia("Felipe", 68, "tarde", "cardiologia")
+        ];
 
+        for($i=0; $i<count($medicos); $i++){
+            $medicos[$i]->mostrarDatos();
+        }
+
+        echo "<br>";
+
+        $urgenciasMas60=0;
+        for($i=0; $i<count($medicos); $i++){
+            //Con "instanceof" comprobamos si el objeto es de una clase determinada
+            if($medicos[$i] instanceof Urgencia && $medicos[$i]->getTurno()=="tarde" && $medicos[$i]->getEdad()>60){
+                $urgenciasMas60++;
+            }
+        }
+        echo "Hay $urgenciasMas60 medicos de urgencias con mas de 60 años en turno de tarde <br><br>";
+
+
+        $numMinimoPacientes=5;
+        echo "Los datos de los medicos que tienen $numMinimoPacientes o mas pacientes: <br>";
+
+        foreach($medicos as $medico){
+            if($medico instanceof Familia && $medico->getNuMPacientes()>=$numMinimoPacientes){
+                $medico->mostrarDatos();
+            }
+        }
     ?>
 
     <br><br><br>
 
     <h3>Ejercicio 4</h3>
     <?php
+        $cestaCompra=[
+            new Alimentacion(1, 23.29, "Platano", "3-12-2025"),
+            new Alimentacion(2, 33.79, "Sandia", "4-1-2025"),
+            new Alimentacion(3, 43.69, "Melon", "6-7-2025"),
+            new Alimentacion(4, 3.99, "Manzana", "21-4-2025"),
+            new Alimentacion(5, 4.29, "Pera", "15-6-2025"),
+            new Electronica(6, 400.29, "PS4 Pro", "2 años"),
+            new Electronica(7, 720.29, "PS5 Slim", "3 años"),
+            new Electronica(8, 900.29, "PS5 Pro", "4 años"),
+            new Electronica(9, 2259.29, "Ordenador Gaming", "5 años"),
+            new Electronica(10, 1339.29, "Television 4k", "10 años")
+        ];
+
+        $precioAlimientos=0;
+        $precioElectronica=0;
+
+        echo "Cesta de la compra:<br>";
+
+        foreach($cestaCompra as $producto){
+            $producto->mostrarDatos();
+            if($producto instanceof Alimentacion && $producto->getPrecio()){
+                $precioAlimientos+=$producto->getPrecio();
+            }else if($producto instanceof Electronica && $producto->getPrecio()){
+                $precioElectronica+=$producto->getPrecio();
+            }
+        }
+
+        echo "Se ha gastado un total de " . ($precioAlimientos+$precioElectronica) . "€<br>";
+        echo "En alimentacion se ha gastado $precioAlimientos €<br>";
+        echo "En electronica se ha gastado $precioElectronica €<br>";
 
     ?>
 
--- PHP Carmen/UD3/Hoja03_PHP_05Funciones.php
+++ PHP Carmen/UD3/Hoja03_PHP_05Funciones.php
@@ -115,7 +115,7 @@
 
 
 
-    <!-- EJERCICIO 2 -->
+    <!-- EJERCICIO 3 -->
     <?php
         abstract class Medico{
             protected $nombre;
@@ -127,6 +127,18 @@
                 $this->edad=$edad;
                 $this->turno=$turno;
             }
+
+            abstract public function mostrarDatos():void;
+            
+            //metodos GET de edad y turno para obtener el numero de medicos con mas de 60 años en turno de tarde
+            public function getEdad():int{
+                return $this->edad;
+            }
+
+            public function getTurno():string{
+                return $this->turno;
+            }
+
         }
 
         class Familia extends Medico{
@@ -136,10 +148,77 @@
                 parent::__construct($nombre, $edad, $turno);
                 $this->num_pacientes=$num_pacientes;
             }
+
+            public function getNuMPacientes():int{
+                return $this->num_pacientes;
+            }
+
+            //Mostrarlos datos del medico de familia
+            public function mostrarDatos():void{
+                echo "<p>Medico de Familia: $this->nombre, Edad: $this->edad, Turno: $this->turno, Numero de pacientes: $this->num_pacientes</p>";
+            }
         }
 
         class Urgencia extends Medico{
             private $unidad;
+
+            public function __construct(string $nombre, int $edad, string $turno, string $unidad){
+                parent::__construct($nombre, $edad, $turno);
+                $this->unidad=$unidad;
+            }
+
+            public function mostrarDatos():void{
+                echo "<p>Medico de Urgencia: $this->nombre, Edad: $this->edad, Turno: $this->turno, Unidad: $this->unidad</p>";
+            }
+        }
+    ?>
+
+
+
+    <!-- EJERCICIO 4 -->
+    <?php
+        abstract class Productos{
+            protected $codigo;
+            protected $precio;
+            protected $nombre;
+
+            public function __construct(int $codigo, float $precio, string $nombre){
+                $this->codigo=$codigo;
+                $this->precio=$precio;
+                $this->nombre=$nombre;
+            }
+
+            abstract public function mostrarDatos():void;
+
+            public function getPrecio():float{
+                return $this->precio;
+            }
+        }
+
+        class Alimentacion extends Productos{
+            private $fechaCaducidad;
+
+            public function __construct(int $codigo, float $precio, string $nombre, string $fechaCaducidad){
+                parent::__construct($codigo, $precio, $nombre);
+                $this->fechaCaducidad=$fechaCaducidad;
+            }
+
+            public function mostrarDatos():void{
+                echo "<p>Nombre del alimento: $this->nombre, Precio del producto: $this->precio €, Fecha de caducidad: $this->fechaCaducidad</p>";
+            }
+        }
+
+        class Electronica extends Productos{
+            private $plazoGarantia;
+
+            public function __construct(int $codigo, float $precio, string $nombre, string $plazoGarantia){
+                parent::__construct($codigo, $precio, $nombre);
+                $this->plazoGarantia=$plazoGarantia;
+            }
+
+            public function mostrarDatos():void{
+                echo "<p>Nombre del electrodomestico: $this->nombre, Precio del producto: $this->precio €, Plazo de garantia: $this->plazoGarantia</p>";
+            }
         }
     ?>
 </body>
