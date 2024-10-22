d81b460c9fbb84877254a28f4d9a45d567b249bd
FranPcBaseDAW <fran.5oct99@gmail.com>
2024-10-22 16:26:03 +0200

    a

--- JavaScript Alejandro/UD2/Bingo/bingo.js
+++ JavaScript Alejandro/UD2/Bingo/bingo.js
@@ -4,18 +4,53 @@ $bingo=(function(){
     let cartonJugadores=[];
     let bolasSacadas=[];
 
+
+    //cambiar a sacar 90 numeros y ordenarlos de forma aleatoria con sort random
     function siguientBola(){
-        let numAleatorio;
-        while(bolasSacadas.length<50){
-            numAleatorio=(Math.floor(Math.random()*(100-1))+1);
-            for(bola of bolasSacadas){
-                if(bolasSacadas[bola]!=numAleatorio){
-                    bolasSacadas.push(numAleatorio);
-                }
-            }
+        bolasAleatorias=[];
+
+        for (let i=1; i<=90; i++){
+            bolasAleatorias.push(i);
         }
+
+        bolasAleatorias.sort(Math.random()-0.5)
+
+        bolasSacadas.push(bolasAleatorias.shift());
+
+
+        // let numAleatorio;
+        // let bolaSacada=false;
+
+        // // Saco un numero aleatorio hasta que no este en bolasSacadas[]
+        // do{
+        //     numAleatorio=(Math.floor(Math.random()*(90-1))+1);
+        //     bolaSacada=true;
+
+        //     for(let bola of bolasSacadas){
+        //         if(bola===numAleatorio){
+        //             bolaSacada=false
+        //             break;
+        //         }
+        //     }
+        // }while(!bolaSacada);
+
+        // bolasSacadas.push(numAleatorio);
     }
 
-    
+
+
+    // generar 90 numeros y ordenarlos de forma aleatoria con sort (a,b)=>{} mathRadom()-0,5
+    function generarCartones(){
+        let carton=[[],[],[]];
+
+        let aleatorio=[];
+        for(let i=1; i<=90; i++){
+            aleatorio.push(i);
+        }
+
+        aleatorio.sort(()=>Math.random()-0.5)
+
+        }
+
     return ;
 })();
