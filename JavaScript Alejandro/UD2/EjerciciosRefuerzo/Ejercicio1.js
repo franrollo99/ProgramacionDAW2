58c4847bb246a2791aa0f62fb3975e1bf1563de2
FranPcBaseDAW <fran.5oct99@gmail.com>
2024-11-04 16:25:08 +0100

    a

--- JavaScript Alejandro/UD2/EjerciciosRefuerzo/Ejercicio1.js
+++ JavaScript Alejandro/UD2/EjerciciosRefuerzo/Ejercicio1.js
@@ -1,16 +1,16 @@
-function comprobarAnagrama(palabra1, palabra2){
-    if(palabra1===palabra2){
+function comprobarAnagrama(palabra1, palabra2) {
+    if (palabra1 === palabra2) {
         alert('Las palabras no son anagramas porque son iguales');
         return;
-    }else if(palabra1.length!==palabra2.length){
+    } else if (palabra1.length !== palabra2.length) {
         alert('Las palabras no son anagramas porque tienen diferente numero de caracteres');
         return;
     }
 
-    let letras1=letrasPalabra(palabra1);
-    let letras2=letrasPalabra(palabra2);
+    let letras1 = letrasPalabra(palabra1);
+    let letras2 = letrasPalabra(palabra2);
 
-    for(let letra1 in letras1) {
+    for (let letra1 in letras1) {
         let encontrado = false;
         for (let letra2 in letras2) {
             if (letra1 === letra2 && letras1[letra1] === letras2[letra2]) {
@@ -27,18 +27,18 @@ function comprobarAnagrama(palabra1, palabra2){
     return alert('Las palabras ' + palabra1 + ' y ' + palabra2 + ' son anagramas');;
 }
 
-function letrasPalabra(palabra){
-    let letras={};
-    
-    for(let i=0; i<palabra.length; i++){
-        let letra=palabra[i];
-        letras[letra]=(letras[letra] || 0) + 1; // Si letras[letra] es cualquier valor considerado falso (0, undefined, null, NaN, "", false), usa el segundo valor, el 0
+function letrasPalabra(palabra) {
+    let letras = {};
+
+    for (let i = 0; i < palabra.length; i++) {
+        let letra = palabra[i];
+        letras[letra] = (letras[letra] || 0) + 1; // Si letras[letra] es cualquier valor considerado falso (0, undefined, null, NaN, "", false), usa el segundo valor, el 0
     }
 
     return letras;
 }
 
-let palabra1=prompt('Introduce una palabra').toLocaleLowerCase();
-let palabra2=prompt('Introduce otra palabra').toLocaleLowerCase();
+let palabra1 = prompt('Introduce una palabra').toLowerCase();
+let palabra2 = prompt('Introduce otra palabra').toLowerCase();
 
 comprobarAnagrama(palabra1, palabra2);
--- JavaScript Alejandro/UD2/EjerciciosRefuerzo/Ejercicio4.js
+++ JavaScript Alejandro/UD2/EjerciciosRefuerzo/Ejercicio4.js
@@ -2,22 +2,22 @@ function expresionesEquilibradas(expresion) {
     let almacenarSimbolos = [];
     let simbolosApertura = ['(', '{', '[',];
     let simbolosCierre = [')', '}', ']'];
-    
+
     for (let i = 0; i < expresion.length; i++) {
         let caracter = expresion[i];
-        
-        if (simbolosApertura[i]===caracter) {
+
+        if (simbolosApertura[i] === caracter) {
             almacenarSimbolos.push(caracter);
-        } else if(simbolosCierre.includes(caracter)){
-            if(almacenarSimbolos===0){
+        } else if (simbolosCierre.includes(caracter)) {
+            if (almacenarSimbolos === 0) {
                 return "La expresion no es equilibrada";
             }
         }
     }
-    
-    if(almacenarSimbolos.length === 0){
+
+    if (almacenarSimbolos.length === 0) {
         return "La expresion es equilibrada";
-    }else{
+    } else {
         return "La expresion no es equilibrada";
     }
 }
