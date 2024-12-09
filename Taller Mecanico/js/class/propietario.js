25a5464205908bf8d9d07b992d3d6f3c57538612
FranPcClaseServidor <fran.5oct99@gmail.com>
2024-12-09 17:50:22 +0100

    a

--- Taller Mecanico/js/class/propietario.js
+++
@@ -1,9 +0,0 @@
-class Propietario{
-    constructor(nombre, telefono, email){
-        this.nombre=nombre;
-        this.telefono=telefono;
-        this.email=email;
-    }
-}
-
-export default Propietario;
