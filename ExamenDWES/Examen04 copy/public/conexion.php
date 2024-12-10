5825dd2a228ac5bdc3c6b096fe5db06a87540783
Fran <fran.5oct99@gmail.com>
2024-12-10 15:28:25 +0100

    a

--- ExamenDWES/Examen04 copy/public/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../vendor/autoload.php";
-use Src\Clases\ConexionBD;
-
-$conexion = conexionBD::getConexion();
-if($conexion instanceof PDO){
-    echo "Conexion establecida con exito";
-}
-?>
