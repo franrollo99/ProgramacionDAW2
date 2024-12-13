71ea8531dca7bf64344332cf92cd1e7219acc044
Fran <fran.5oct99@gmail.com>
2024-12-13 17:35:02 +0100

    a

--- JavaScript Alejandro/UD3/Ejercicio 6/script.js
+++ JavaScript Alejandro/UD3/Ejercicio 6/script.js
@@ -1,7 +1,9 @@
 import alumnos from './datos.js';
 
-const contenedor = document.querySelector('.listaAlumnos');
+const contenedor1 = document.querySelector('.listaAlumnos');
+const contenedor2 = document.querySelector('.alumnosSeleccionados');
 const ciclos = document.getElementById('ciclos');
+
 
 // Filtro por ciclo, en caso de ser todos devuelve todos los alumnos, sino devuelve los alumnos del ciclo seleccionado
 function filtrarAlumnosPorCiclo(filtro) {
@@ -13,10 +15,29 @@ function filtrarAlumnosPorCiclo(filtro) {
 
 // HTML para mostrar los alumnos en funcion del filtro
 function HTMLListarAlumnos(alumnosFiltrados) {
-    contenedor.innerHTML = alumnosFiltrados.map(alumno => `
-        <div><p>${alumno.nombre}</p></div>
-        <div><p>${alumno.ciclo}</p></div>
+    contenedor1.innerHTML = alumnosFiltrados.map(alumno => `
+        <div class="fila" data-id="${alumno.alumnoId}">
+            <div><p>${alumno.nombre}</p></div>
+            <div><p>${alumno.ciclo}</p></div>
+        </div>
     `).join('');
+}
+
+// HTML para mostrar los alumnos seleccionados
+// REVISAR
+function HTMLAlumnosSeleccionados(alumnosSeleccionados) {
+    contenedor2.innerHTML = alumnosSeleccionados.map(alumno => `
+        <div class="fila" data-id="${alumno.alumnoId}">
+            <div><p>${alumno.nombre}</p></div>
+            <div><p>${alumno.ciclo}</p></div>
+            <div><button data-action="subir">+</button><button data-action="bajar">-</button></div>
+        </div>
+    `).join('');
+}
+
+// HTML para mostrar un JSON con los datos de los alumnos seleccionados al pulsar enviar
+function HTMLAlumnosEnviar(alumnosSeleccionados) {
+
 }
 
 window.addEventListener('load', function(){
@@ -30,4 +51,58 @@ window.addEventListener('load', function(){
         const alumnosFiltrados = filtrarAlumnosPorCiclo(cicloSeleccionado);
         HTMLListarAlumnos(alumnosFiltrados);
     });
+
+    // Evento para seleccionar un alumno
+    let filaSeleccionada=null;
+    // let alumnosSeleccionados = [];
+    let id;
+
+
+    // Delego el evento de seleccionar una fila al contenedor en lugar de al div donde se muestran
+    // alumnos en funcion del filtro porque estoy trabajando con elementos generados dinamicamente
+    document.querySelector('form').addEventListener('click', function(event) {
+
+        // Verificamos si el click fue sobre una fila
+        if (event.target.closest('.fila')) {
+            const fila = event.target.closest('.fila');
+            id = fila.dataset.id;
+
+            // Cambiar la selección de fila
+            if(filaSeleccionada === fila){
+                fila.classList.toggle('filaSeleccionada');
+                filaSeleccionada = null;
+            }else if (filaSeleccionada) {
+                fila.classList.toggle('filaSeleccionada');
+                filaSeleccionada.classList.toggle('filaSeleccionada');
+                filaSeleccionada = fila;
+            } else {
+                fila.classList.toggle('filaSeleccionada');
+                filaSeleccionada = fila;
+            }
+
+            
+        }
+
+        // Evento para agregar un alumno seleccionado a la otra tabla
+        if (event.target.closest('#btnAgregar') && filaSeleccionada){
+            event.preventDefault();
+            console.log(id)
+            // let alumno1 = alumnos.find(alumno => alumno.alumnoId === id);
+            // console.log(alumno1.nombre);
+            // alumnosSeleccionados.push(alumnos.find(alumno => alumno.alumnoId === id));
+            // console.log(alumnosSeleccionados);
+
+        }
+            
+            
+    });
+
 });
+
+
+
+
+
+
+
+            
