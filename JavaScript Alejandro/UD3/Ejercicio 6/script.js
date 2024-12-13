6f952d89b20b832202dbe0d69a078a31b77c0e23
franrollo99 <fran.5oct99@gmail.com>
2024-12-13 01:42:18 +0100

    a

--- JavaScript Alejandro/UD3/Ejercicio 6/script.js
+++ JavaScript Alejandro/UD3/Ejercicio 6/script.js
@@ -1,49 +1,33 @@
 import alumnos from './datos.js';
 
-
-let alumnosfiltrados;
 const contenedor = document.querySelector('.listaAlumnos');
+const ciclos = document.getElementById('ciclos');
 
-
-function filtroAlumnos(cicloSeleccionado){
-    let resultado = alumnos.filter(alumno => alumno.ciclo.toLowerCase() == cicloSeleccionado.toLowerCase());
-    return resultado;
-}
-
-function HTMLListaAlumnos(alumnosFiltrados){
-    contenedor.innerHTML='';
-    contenedor.innerHTML += alumnosFiltrados.map(alumno=>`
-            <div><p>${alumno.nombre}</p></div>
-            <div><p>${alumno.ciclo}</p></div>
-        `).join('');
-}
-
-
-
-document.getElementById('ciclos').addEventListener('change', function(){
-    const cicloSeleccionado =  document.getElementById('ciclos').value;
-    
-
-    switch(cicloSeleccionado){
-        case 'todos':
-
-            break;
-        case 'asir':
-            alumnosfiltrados=filtroAlumnos(cicloSeleccionado);
-            HTMLListaAlumnos(alumnosfiltrados);
-            break;
-        case 'dam':
-            alumnosfiltrados=filtroAlumnos(cicloSeleccionado);
-            HTMLListaAlumnos(alumnosfiltrados);
-            break;
-        case 'daw':
-            alumnosfiltrados=filtroAlumnos(cicloSeleccionado);
-            HTMLListaAlumnos(alumnosfiltrados);
-            break;
+// Filtro por ciclo, en caso de ser todos devuelve todos los alumnos, sino devuelve los alumnos del ciclo seleccionado
+function filtrarAlumnosPorCiclo(filtro) {
+    if (filtro === 'todos') {
+        return alumnos;
     }
+    return alumnos.filter(alumno => alumno.ciclo.toLowerCase() === filtro.toLowerCase());
+}
 
-    
+// HTML para mostrar los alumnos en funcion del filtro
+function HTMLListarAlumnos(alumnosFiltrados) {
+    contenedor.innerHTML = alumnosFiltrados.map(alumno => `
+        <div><p>${alumno.nombre}</p></div>
+        <div><p>${alumno.ciclo}</p></div>
+    `).join('');
+}
 
+window.addEventListener('load', function(){
 
-    // console.log(filtroAlumnos(cicloSeleccionado));
+    // Mostrar todos los alumnos al cargar la página
+    const alumnosIniciales = filtrarAlumnosPorCiclo('todos');
+    HTMLListarAlumnos(alumnosIniciales);
+
+    ciclos.addEventListener('change', function(){
+        const cicloSeleccionado = ciclos.value;
+        const alumnosFiltrados = filtrarAlumnosPorCiclo(cicloSeleccionado);
+        HTMLListarAlumnos(alumnosFiltrados);
+    });
 });
