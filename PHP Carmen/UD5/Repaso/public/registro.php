f06bdabcc5d86c823ff538cb73012a1376bef789
FranPcClaseServidor <fran.5oct99@gmail.com>
2024-12-19 18:09:35 +0100

    a

--- PHP Carmen/UD5/Repaso/public/registro.php
+++ PHP Carmen/UD5/Repaso/public/registro.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+    <link rel="stylesheet" href="../estilo/estiloRegistro.css">
 </head>
 <body>
-    
+    <h1>Formulario de registro</h1>
+    <form method="post">
+        <label for="usuario">Usuario</label>
+        <input type="text" name="usuario" id="usuario">
+        <label for="contraseña">Contraseña</label>
+        <input type="password" name="contraseña" id="contraseña">
+        <label for="confirmarPassword">Confirmar contraseña</label>
+        <input type="password" name="confirmarPassword" id="confirmarPassword">
+        <input type="submit" value="Registrarte">
+    </form>
 </body>
 </html>
--- PHP Carmen/UD5/Repaso/src/clases/funciones.php
+++ PHP Carmen/UD5/Repaso/src/clases/funciones.php
@@ -3,5 +3,28 @@ namespace Fran\Src\clases;
 use PDO;
 use Fran\Src\clases\ConexionBD;
 
+final class FuncionesBD{
 
+    function validarCredenciales($usuario, $contraseña){
+        $conexion = ConexionBD::getConexion();
+        $consulta = $conexion->prepare("SELECT usuario, contrasena from usuarios where usuario=:usuario");
+        $consulta->bindParam(":usuario", $usuario);
+        $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+
+
+        if($consulta->execute){
+            // hash contraseña
+            if($resultado['contrasena'] == $resultado){
+                return true;
+            }
+            return false;
+        }{
+            return false;
+        }
+    }
+
+    function registrarUsuario($usuario, $contraseña){
+
+    }
+}
 ?>
