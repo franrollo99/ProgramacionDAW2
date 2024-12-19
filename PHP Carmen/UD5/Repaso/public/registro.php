c196af0e894a0f1b3b81473f2a1c0f0b9a0850b0
FranPcClaseServidor <fran.5oct99@gmail.com>
2024-12-19 18:20:09 +0100

    a

--- PHP Carmen/UD5/Repaso/public/registro.php
+++ PHP Carmen/UD5/Repaso/public/registro.php
@@ -7,15 +7,34 @@
     <link rel="stylesheet" href="../estilo/estiloRegistro.css">
 </head>
 <body>
+
+    <?php
+    require_once 
+    ?>
+
     <h1>Formulario de registro</h1>
     <form method="post">
         <label for="usuario">Usuario</label>
-        <input type="text" name="usuario" id="usuario">
+        <input type="text" name="usuario" id="usuario" required>
         <label for="contraseña">Contraseña</label>
-        <input type="password" name="contraseña" id="contraseña">
-        <label for="confirmarPassword">Confirmar contraseña</label>
-        <input type="password" name="confirmarPassword" id="confirmarPassword">
-        <input type="submit" value="Registrarte">
+        <input type="password" name="contraseña" id="contraseña" required>
+        <label for="confirmarContraseña = $_POST['contraseña'];">Confirmar contraseña</label>
+        <input type="password" name="confirmarContraseña" id="confirmarContraseña" required>
+        <input type="submit" value="Registrarte" name="registro">
     </form>
+
+    <?php
+    $usuario = $_POST['usuario'] ?? null;
+    $contraseña = $_POST['contraseña'] ?? null;
+    $confirmarContraseña = $_POST['confirmarContraseña'] ?? null;
+    $hashContraseña = password_hash($contraseña, PASSWORD_BCRYPT);
+
+    if(isset($_POST['registro']) && $contraseña === $confirmarContraseña){
+        echo "hola";
+    }else{
+        echo "La contraseña no coincide";
+    }
+    ?>
+
 </body>
 </html>
