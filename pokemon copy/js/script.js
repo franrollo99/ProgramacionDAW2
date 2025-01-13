f135ade03f835b07aa03c36ab39b896169c41835
franrollo99 <fran.5oct99@gmail.com>
2025-01-13 11:25:13 +0100

    a

--- pokemon copy/js/script.js
+++
@@ -1,5 +0,0 @@
-// Buscar dato del pokemon dependiendo del numero o nombre
-function buscarPokemon(contenedor){
-    let inputId = 'pokemonInput' + contenedor;
-    let nombrePokemon = document.getElementById(inputId);
-}
