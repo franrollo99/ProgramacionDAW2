872d4da8bdf27001abd60aa16f4bbbbafd9db6f4
FranPcClaseServidor <fran.5oct99@gmail.com>
2025-01-16 18:20:49 +0100

    a

--- PHP Carmen/UD6/Hoja06_MVC/resources/views/animales/index.blade.php
+++ PHP Carmen/UD6/Hoja06_MVC/resources/views/animales/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.plantilla')
-@section('titulo', 'Zoologico-Listado de animales')
+
+@section('titulo', 'Zoologico - Listado de animales')
+
 @section('contenido')
-<h1 class="text-3xl font-bold underline">Listado de animales</h1>
+    <h1 class="text-3xl font-bold underline">Listado de animales</h1>
+    @foreach ($animales as $animal)
+        <p>Especie: {{$animal['especie']}}</p>
+    @endforeach
+
 @endsection
