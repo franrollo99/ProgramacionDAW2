859aed9e451268bd9c4026cc697ca6e676037164
Fran <fran.5oct99@gmail.com>
2025-03-18 16:26:15 +0100

    a

--- RECUPERACIONES JavaScript/UD2/Ejercicios de lógica/Ejercicio2.js
+++ RECUPERACIONES JavaScript/UD2/Ejercicios de lógica/Ejercicio2.js
@@ -1,16 +1,9 @@
 let numeros = [];
 let numeroIntroducido;
-
-// do{
-//     numeroIntroducido = prompt('Introduzca un número');
-
-//     if(isNaN(numeroIntroducido)){
-//         alert('Debes introducir un numero');
-// 0   }if(!isNaN(numeroIntroducido) && numeroIntroducido != 0){
-//         numeros.push(numeroIntroducido);
-//     }
-
-// }while(numeroIntroducido != '0');
+let numeroMaximo;
+let numeroMinimo;
+let sumaNumeros = 0;
+let mediaNumeros;
 
 do{
     numeroIntroducido = parseInt(prompt('Introduzca un número'));
@@ -25,9 +18,46 @@ do{
 }while(numeroIntroducido !== 0);
 
 if(numeros.length > 0){
-    console.log(numeros);
+    // console.log(numeros);
+    
+    for(let i=0; i<numeros.length; i++){
+        sumaNumeros += numeros[i];
+
+        if(i===0){
+            numeroMaximo = numeros[i];
+            numeroMinimo = numeros[i];
+        }else{
+            if(numeroMaximo < numeros[i]){
+                numeroMaximo = numeros[i];
+            }
+
+            if(numeroMinimo > numeros[i]){
+                numeroMinimo = numeros[i];
+            }
+        }
+    }
+
+    mediaNumeros = sumaNumeros / numeros.length;
+
+    console.log('La suma de todos los numeros es: ' + sumaNumeros);
+    console.log('La media de todos los numeros es: ' + mediaNumeros);
+    console.log('La cantidad de números introducidos es de: ' + numeros.length);
+    console.log('El numero mas alto es: ' + numeroMaximo);
+    console.log('El numero mas bajo es: ' + numeroMinimo);
 }else{
     console.log('No has introducido ningun numero');
 }
 
 
+
+// **********************  Bucle sin parsear a entero  **********************
+// do{
+//     numeroIntroducido = prompt('Introduzca un número');
+
+//     if(isNaN(numeroIntroducido)){
+//         alert('Debes introducir un numero');
+// 0   }if(!isNaN(numeroIntroducido) && numeroIntroducido != 0){
+//         numeros.push(numeroIntroducido);
+//     }
+
+// }while(numeroIntroducido != '0');
