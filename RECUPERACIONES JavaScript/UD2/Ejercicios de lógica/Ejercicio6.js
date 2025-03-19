acce1f07664cf4bf46987283a710da2c29318730
franrollo99 <fran.5oct99@gmail.com>
2025-03-19 14:27:10 +0100

    a

--- RECUPERACIONES JavaScript/UD2/Ejercicios de lógica/Ejercicio6.js
+++ RECUPERACIONES JavaScript/UD2/Ejercicios de lógica/Ejercicio6.js
@@ -2,5 +2,6 @@ let entrada1;
 
 do{
     entrada1 = parseInt(prompt('Introduce el numero de la figura que quieres dibujar: \n1-Cuadrado \n2-Rectangulo \n3-Triangulo \n4-Rombo \n0-Salir'));
+    
 
 }while(entrada1 !==0)
