bdaa6c5ca04d09929927ddd000bfefa0ba0faded
franrollo99 <fran.5oct99@gmail.com>
2025-03-21 02:24:49 +0100

    a

--- RECUPERACIONES JavaScript/UD2/Ejercicios de lógica/Ejercicio12.js
+++ RECUPERACIONES JavaScript/UD2/Ejercicios de lógica/Ejercicio12.js
@@ -1,1 +1,29 @@
-///////////////////////////////////////// TERMINAR
+let entrada;
+
+let numSecreto = parseInt(Math.random()*100);
+
+for(let i=0; i<5; i++){
+    do{
+        entrada = parseInt(prompt('Adivina el numero secreto entre 0 y 100'));
+    
+        if(isNaN(entrada)){
+            alert('Debes introducir un numero');
+        }
+        
+    }while(isNaN(entrada))
+
+    if(entrada === numSecreto){
+        console.log('¡¡¡¡¡ HAS ACERTADO !!!!!');
+        break;
+    }else{
+        if(i === 4){
+            console.log('Has perdido :( \nEl numero secreto era el ' + numSecreto);
+            break;
+        }
+        if(entrada>numSecreto){
+            console.log('El numero secreto es menor que ' + entrada);
+        }else{
+            console.log('El numero secreto es mayor que ' + entrada);
+        }
+    }
+}
