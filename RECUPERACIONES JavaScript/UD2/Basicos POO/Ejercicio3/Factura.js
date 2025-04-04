c3a9f442a2f3b8c588f6011a32349c47ece013f0
franrollo99 <fran.5oct99@gmail.com>
2025-04-04 14:20:02 +0200

    a

--- RECUPERACIONES JavaScript/UD2/Basicos POO/Ejercicio3/Factura.js
+++ RECUPERACIONES JavaScript/UD2/Basicos POO/Ejercicio3/Factura.js
@@ -1,11 +1,11 @@
 class Factura {
 
     // pagada no la paso en el constructor porque la pongo con valor predeterminado false
-    constructor(clienteNIF, fecha, hora) {
+    constructor(clienteNIF, fecha, hora, pagado) {
         this.clienteNIF = clienteNIF;
         this.fecha = fecha;
         this.hora = hora;
-        this.pagada = false;
+        this.pagada = pagado ? 'Si' : 'No';
         this.lineas = [];
     }
 
@@ -18,20 +18,24 @@ class Factura {
     }
 
     imprimirFactura() {
-        const resultado = '';
+        let resultado = '';
         resultado += `<p>NIF cliente: ${this.clienteNIF}</p>`;
         resultado += `<p>Fecha: ${this.fecha}</p>`;
         resultado += `<p>Hora: ${this.hora}</p>`;
         resultado += `<p>Pagada: ${this.clienteNIF ? 'Si' : 'No'}</p>`;
-        for (linea of this.lineas) {
-            resultado += `<li>: ${linea}</li>`;
+        resultado += '<ul>';
+
+        for (let linea of this.lineas) {
+            resultado += `<li>${linea.toString()}</li>`;
         }
+    
+        resultado += '</ul>';
 
         return resultado;
     }
 
     agregarLinea(concepto, cantidad, precio) {
-        let nuevaLinea = '-Concepto: ' + concepto + ' - Cantidad: ' + cantidad + ' -Precio: ' + precio;
+        let nuevaLinea = '-Concepto: ' + concepto + ' -Cantidad: ' + cantidad + ' -Precio: ' + precio;
         this.lineas.push(nuevaLinea);
 
         return null;
--- RECUPERACIONES JavaScript/UD2/Basicos POO/Ejercicio3/index.js
+++ RECUPERACIONES JavaScript/UD2/Basicos POO/Ejercicio3/index.js
@@ -2,6 +2,8 @@ import Factura from './Factura.js';
 import Linea from './Linea.js';
 import Utilidades from './Utilidades.js';
 
+let factura;
+const contenedorFactura = document.getElementById('factura');
 const formCrearFactura = document.getElementById('form-crearFactura');
 const formAñadirLinea = document.getElementById('form-lineasFactura');
 
@@ -10,18 +12,37 @@ window.addEventListener('load', () => {
     formCrearFactura.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const nifCliente = document.getElementById('clienteNIF').value;
+        const clienteNIF = document.getElementById('clienteNIF').value;
         const fecha = document.getElementById('fecha').value;
         const hora = document.getElementById('hora').value;
         const pagado = document.getElementById('pagada').checked;
 
-        console.log(nifCliente);
-        console.log(fecha);
-        console.log(hora);
-        console.log(pagado);
+        factura = new Factura(clienteNIF, fecha, hora, pagado);
+
+        formCrearFactura.reset();
     })
+
+    formAñadirLinea.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const concepto = document.getElementById('concepto').value;
+        const cantidad = document.getElementById('cantidad').value;
+        const precio = document.getElementById('precio').value;
+
+        factura.agregarLinea(concepto, cantidad, precio);
+    });
+
+    document.getElementById('btn-eliminarLinea').addEventListener('click', () => {
+        factura.agregarLinea(concepto, cantidad, precio);
+    });
     
     document.getElementById('imprimirFactura').addEventListener('click', () => {
-
+        contenedorFactura.innerHTML = '';
+        
+        if(factura){
+            contenedorFactura.innerHTML += factura.imprimirFactura();    
+        }else{
+            contenedorFactura.innerHTML += "<p>No existe ninguna factura</p>";    
+        }
     })
 });
