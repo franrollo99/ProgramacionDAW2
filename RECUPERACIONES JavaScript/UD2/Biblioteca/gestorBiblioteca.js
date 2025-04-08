fde8cb0f2c53a1f910ab9bd119bd36769534560f
franrollo99 <fran.5oct99@gmail.com>
2025-04-08 17:10:36 +0200

    a

--- RECUPERACIONES JavaScript/UD2/Biblioteca/gestorBiblioteca.js
+++ RECUPERACIONES JavaScript/UD2/Biblioteca/gestorBiblioteca.js
@@ -4,6 +4,7 @@ import Autor from './Autor.js';
 import Biblioteca from './Biblioteca.js';
 
 const contenedorResultados = document.getElementById('app');
+const formBuscador = document.getElementById('form-buscador');
 
 const $biblio = (function(){
     let libros = datos.libros.map(libro => new Libro(libro.libroId, libro.titulo, libro.ISBN, libro.autorId, libro.bibliotecaId, libro.prestamos));
@@ -20,10 +21,11 @@ const $biblio = (function(){
 
         autores.forEach(autor => {
             resultado += `
-                <div id="autor${autor.autorId}">
-                    <p>Nombre: ${autor.nombre}</p>
-                    <p>Nacionalidad: ${autor.nacionalidad}</p>
-                    <p>Biografia: ${autor.biografia}</p>
+                <div class="contenedorListado" id="autor${autor.autorId}">
+                    <p><b>Nombre:</b> ${autor.nombre}</p>
+                    <p><b>Nacionalidad:</b> ${autor.nacionalidad}</p>
+                    <p><b>Biografia:</b> ${autor.biografia}</p>
+                    <button id="btn-crearAutor${autor.autorId}">Crear</button>
                     <button id="btn-verAutor${autor.autorId}">Ver</button>
                     <button id="btn-editarAutor${autor.autorId}">Editar</button>
                     <button id="btn-borrarAutor${autor.autorId}">Borrar</button>
@@ -39,9 +41,10 @@ const $biblio = (function(){
 
         bibliotecas.forEach(biblioteca => {
             resultado += `
-                <div id="biblioteca${biblioteca.bibliotecaId}">
-                    <p>Nombre: ${biblioteca.nombre}</p>
-                    <p>Ubicacion: ${biblioteca.ubicacion}</p>
+                <div class="contenedorListado" id="biblioteca${biblioteca.bibliotecaId}">
+                    <p><b>Nombre:</b> ${biblioteca.nombre}</p>
+                    <p><b>Ubicacion:</b> ${biblioteca.ubicacion}</p>
+                    <button id="btn-crearBiblioteca${biblioteca.bibliotecaId}">Crear</button>
                     <button id="btn-verBiblioteca${biblioteca.bibliotecaId}">Ver</button>
                     <button id="btn-editarBiblioteca${biblioteca.bibliotecaId}">Editar</button>
                     <button id="btn-borrarBiblioteca${biblioteca.bibliotecaId}">Borrar</button>
@@ -57,9 +60,10 @@ const $biblio = (function(){
 
         libros.forEach(libro => {
             resultado += `
-                <div id="libro${libro.libroId}">
-                    <p>Titulo: ${libro.titulo}</p>
-                    <p>ISBN: ${libro.ISBN}</p>
+                <div class="contenedorListado" id="libro${libro.libroId}">
+                    <p><b>Titulo:</b> ${libro.titulo}</p>
+                    <p><b>ISBN:</b> ${libro.ISBN}</p>
+                    <button id="btn-crearLibro${libro.libroId}">Crear</button>
                     <button id="btn-verLibro${libro.libroId}">Ver</button>
                     <button id="btn-editarLibro${libro.libroId}">Editar</button>
                     <button id="btn-borrarLibro${libro.libroId}">Borrar</button>
@@ -70,16 +74,32 @@ const $biblio = (function(){
         return resultado;
     }
 
+
     function buscarLibrosPorTitulo(titulo){
-        
+        // toLowerCase() devuelve un numero: 0 si empieza con la cadena, >0 si contiene la cadena, y -1 si no contiene la cadena
+        return libros.filter(libro => libro.titulo.toLowerCase().indexOf(titulo.toLowerCase()) !== -1); // Si libro.titulo contiene la cadena de titulo devuelve ese libro
     }
 
     function buscarLibrosPorAutor(nombre){
-        
+        let idsAutores = autores // Saco los ids de los autores
+            .filter(autor => autor.nombre.toLowerCase().indexOf(nombre.toLowerCase()) !== -1)
+            .map(autor => autor.autorId);
+
+        return libros.filter(libro => // Filtro los libros por id de autor
+            idsAutores.indexOf(libro.autorId) !== -1
+        );
     }
 
     function generarHTMLResultadoBuscador(resultados){
+        const resultadoHTML = '';
 
+        if(resultados){
+            console.log(resultados);
+            return null;
+        }
+
+        console.log('nada');
+        return null;
     }
 
     function buscarLibro(libroId){
@@ -172,6 +192,27 @@ window.addEventListener('load', () =>{
 
         contenedorResultados.innerHTML = '';
         contenedorResultados.innerHTML += resultado;
+    });
+
+    formBuscador.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let resultadoBuscador;
+        const filtro = document.getElementById('nombre').value;
+        const tipoLibro = document.getElementById('libro').checked;
+
+        if(filtro){
+            if(tipoLibro){
+                resultadoBuscador = $biblio.buscarLibrosPorTitulo(filtro);
+            }else{
+                resultadoBuscador = $biblio.buscarLibrosPorAutor(filtro);
+            }
+        }
+
+        // console.log(resultadoBuscador);
+        $biblio.generarHTMLResultadoBuscador(resultadoBuscador);
+
+        formBuscador.reset();
     });
 
 
