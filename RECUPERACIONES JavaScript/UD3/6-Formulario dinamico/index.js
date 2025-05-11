fbd27f54338d1999f3fd6b307394fa79c5f00c71
franrollo99 <fran.5oct99@gmail.com>
2025-05-11 21:01:34 +0200

    a

--- RECUPERACIONES JavaScript/UD3/6-Formulario dinamico/index.js
+++ RECUPERACIONES JavaScript/UD3/6-Formulario dinamico/index.js
@@ -1,57 +1,219 @@
-const datos = [
-    { alumnoId: 1, nombre: "Ana López", ciclo: "DAW" },
-    { alumnoId: 2, nombre: "Laura Sánchez", ciclo: "ASIR" },
-    { alumnoId: 3, nombre: "Carlos Martínez", ciclo: "DAM" },
-    { alumnoId: 4, nombre: "Juan Pérez", ciclo: "ASIR" },
-    { alumnoId: 5, nombre: "Mario Gómez", ciclo: "DAW" },
-    { alumnoId: 6, nombre: "Lucía Torres", ciclo: "DAM" },
-    { alumnoId: 7, nombre: "Sofía Núñez", ciclo: "ASIR" },
-    { alumnoId: 8, nombre: "Clara Sánchez", ciclo: "DAW" },
-    { alumnoId: 9, nombre: "Manuel Díaz", ciclo: "DAM" },
-    { alumnoId: 10, nombre: "Pedro Torres", ciclo: "DAW" },
-    { alumnoId: 11, nombre: "Elena Ruiz", ciclo: "DAM" },
-    { alumnoId: 12, nombre: "Álvaro Morales", ciclo: "ASIR" }
-]
+import datos from './datos.js';
+
+let alumnos = [...datos];
 
 const app = document.getElementById('app');
+const form = document.getElementById('form');
 
 const filtro = document.getElementById('filtrarPorCiclo');
+const tablas = document.getElementById('listadoYFormulario');
+const agregar = document.getElementById('agregar');
+const eliminar = document.getElementById('eliminar');
+
 const listaAlumnos = document.getElementById('alumnos');
 const listaAlumnosSeleccionados = document.getElementById('alumnosSeleccionados');
 
+const mensajeJSON = document.getElementById('mensajeJSON');
+
+
+
+let idAlumnoSeleccionado;
+let alumnosSeleccionados = [];
+
 window.addEventListener('load', () => {
 
-    listaAlumnos.innerHTML = listarAlumnos(datos);
+    actualizarListado();
 
+    // Evento de filtrado
     filtro.addEventListener('change', () => {
-        const ciclo = filtro.value;
-
-        const alumnos = filtrarAlumnos(ciclo);
-
-        listaAlumnos.innerHTML = listarAlumnos(alumnos);
+        actualizarListado();
     });
 
+    // Evento de seleccion de alumno
+    tablas.addEventListener('click', (e) => {
+        if (e.target.dataset.action !== 'subir' && e.target.dataset.action !== 'bajar') {
+            const alumnoSeleccionado = e.target.closest('.filaAlumno');
+
+            const filasAlumnos = document.querySelectorAll('.filaAlumno');
+
+            if (alumnoSeleccionado) {
+                for (let fila of filasAlumnos) {
+                    if (fila.classList.contains('seleccionado') && fila !== alumnoSeleccionado) {
+                        fila.classList.remove('seleccionado');
+                    }
+                }
+
+                alumnoSeleccionado.classList.toggle('seleccionado');
+                // Cojo el id para trabajar con él y lo parseo a entero ya que al coger un elemento del html se coge 
+                // como string y puede dar problemas si luego se usa para compararse con un entero
+                idAlumnoSeleccionado = parseInt(alumnoSeleccionado.dataset.id);
+
+                if (!alumnoSeleccionado.classList.contains('seleccionado')) {
+                    idAlumnoSeleccionado = null;
+                }
+            }
+        }
+    });
+
+    // Evento de agregar alumno seleccionado al formulario
+    agregar.addEventListener('click', () => {
+        if (idAlumnoSeleccionado) {
+            const alumnoSeleccionado = alumnos.filter(alumno => alumno.alumnoId === idAlumnoSeleccionado);
+            alumnos = alumnos.filter(alumno => alumno.alumnoId !== idAlumnoSeleccionado);
+
+            if(alumnoSeleccionado[0]){
+                alumnosSeleccionados.push(alumnoSeleccionado[0]);
+                
+                actualizarListadoSeleccionados(alumnosSeleccionados);
+                actualizarListado()
+            }
+        }
+    });
+
+    // Evento de eliminar alumno seleccionado al formulario
+    eliminar.addEventListener('click', () => {
+        if (idAlumnoSeleccionado) {
+            const alumnoSeleccionado = alumnosSeleccionados.filter(alumno => alumno.alumnoId === idAlumnoSeleccionado);
+            alumnosSeleccionados = alumnosSeleccionados.filter(alumno => alumno.alumnoId !== idAlumnoSeleccionado);
+
+            if(alumnoSeleccionado[0]){
+                alumnos.push(alumnoSeleccionado[0]);
+
+                actualizarListadoSeleccionados(alumnosSeleccionados);
+                actualizarListado()
+            }
+        }
+    });
+
+    // Evento para subir y bajar los alumnos del formulario
+    listaAlumnosSeleccionados.addEventListener('click', (e) => {
+        if (e.target.dataset.action === 'subir') {
+            const alumnoSeleccionado = parseInt(e.target.closest('.filaAlumno').dataset.id);
+            let i;
+
+            for (let indice in alumnosSeleccionados) {
+                if (alumnosSeleccionados[indice].alumnoId === alumnoSeleccionado) {
+                    // CUIDADO indice ES UN STRING Y HAY QUE PARSEARLO A ENTERO
+                    i = parseInt(indice);
+                }
+            }
+
+            if (i > 0) {
+                let temp = alumnosSeleccionados[i];
+                alumnosSeleccionados[i] = alumnosSeleccionados[i - 1];
+                alumnosSeleccionados[i - 1] = temp;
+
+                actualizarListadoSeleccionados(alumnosSeleccionados);
+            }
+
+        }
+
+        if (e.target.dataset.action === 'bajar') {
+            const alumnoSeleccionado = parseInt(e.target.closest('.filaAlumno').dataset.id);
+            let i;
+
+            for (let indice in alumnosSeleccionados) {
+                if (alumnosSeleccionados[indice].alumnoId === alumnoSeleccionado) {
+                    i = parseInt(indice);
+                }
+            }
+
+            if (i < alumnosSeleccionados.length - 1) {
+                let temp = alumnosSeleccionados[i];
+                alumnosSeleccionados[i] = alumnosSeleccionados[i + 1];
+                alumnosSeleccionados[i + 1] = temp;
+
+                actualizarListadoSeleccionados(alumnosSeleccionados);
+            }
+        }
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const alumnosJSON = {
+            alumnoId: [],
+            nombre: [],
+            ciclo: [],
+            orden: []
+        };
+
+        for(let indice in alumnosSeleccionados){
+            alumnosJSON.alumnoId.push(alumnosSeleccionados[indice].alumnoId);
+            alumnosJSON.nombre.push(alumnosSeleccionados[indice].nombre);
+            alumnosJSON.ciclo.push(alumnosSeleccionados[indice].ciclo);
+            alumnosJSON.orden.push(parseInt(indice) + 1);
+        }
+
+        mensajeJSON.innerHTML = JSON.stringify(alumnosJSON);
+    });
 
 });
 
-function filtrarAlumnos(filtro) {
-    if (filtro === 'todos') {
-        return datos;
-    }
-    return datos.filter(alumno => alumno.ciclo.toLowerCase() === filtro);
-}
-
-function listarAlumnos(alumnos) {
+function actualizarListado() {
+    const ciclo = filtro.value;
     let resultado = '';
+    let alumnosFiltrados = [...alumnos];
 
-    for(let alumno of alumnos){
+    if (ciclo !== 'todos') {
+        alumnosFiltrados = alumnos.filter(alumno => alumno.ciclo.toLowerCase() === ciclo)
+    }
+
+    for (let alumno of alumnosFiltrados) {
         resultado += `
-            <div class="filaAlumno">
+            <div class="filaAlumno" data-id="${alumno.alumnoId}">
                 <div>${alumno.nombre}</div>
                 <div>${alumno.ciclo}</div>
             </div>
         `;
     }
-    
-    return resultado;
+
+    idAlumnoSeleccionado = null;
+    listaAlumnos.innerHTML = resultado;
 }
+
+function actualizarListadoSeleccionados() {
+    let resultado = '';
+    for (let alumno of alumnosSeleccionados) {
+        resultado += `
+            <div class="filaAlumno" data-id="${alumno.alumnoId}">
+                <div>${alumno.nombre}</div>
+                <div>${alumno.ciclo}</div>
+                <div>
+                    <button class="boton" data-action="subir">+</button>
+                    <button class="boton" data-action="bajar">-</button>
+                </div>
+            </div>
+        `;
+    }
+
+    listaAlumnosSeleccionados.innerHTML = resultado;
+}
+
+
+
+// function filtrarAlumnos() {
+//     const ciclo = filtro.value;
+
+//     if (ciclo === 'todos') {
+//         return alumnos;
+//     }
+
+//     return alumnos.filter(alumno => alumno.ciclo.toLowerCase() === ciclo);
+// }
+
+// function listarAlumnos(alumnos) {
+//     let resultado = '';
+
+//     for (let alumno of alumnos) {
+//         resultado += `
+//             <div class="filaAlumno" data-id="${alumno.alumnoId}">
+//                 <div>${alumno.nombre}</div>
+//                 <div>${alumno.ciclo}</div>
+//             </div>
+//         `;
+//     }
+
+//     listaAlumnos.innerHTML = resultado;
+//     idAlumnoSeleccionado = null;
+// }
