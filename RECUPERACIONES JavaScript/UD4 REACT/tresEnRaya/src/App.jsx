e78196ec2a88a36cebdbd2180e775f09bfc4b763
franrollo99 <fran.5oct99@gmail.com>
2025-05-21 14:23:24 +0200

    a

---
+++ RECUPERACIONES JavaScript/UD4 REACT/tresEnRaya/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function Tablero() {
+
+  return (
+    <div className='tablero'>
+      <div className='fila'>
+        {/* <Casilla />
+        <Casilla />
+        <Casilla />
+      </div>
+      <div className='fila'>
+        <Casilla />
+        <Casilla />
+        <Casilla />
+      </div>
+      <div className='fila'>
+        <Casilla />
+        <Casilla />
+        <Casilla /> */}
+      </div>
+    </div>
+  );
+}
+
+export default Tablero;
