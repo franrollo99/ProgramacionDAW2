108bdbafa9977ac3df4c69a14702908b8f55e6ff
franrollo99 <fran.5oct99@gmail.com>
2025-06-10 17:03:50 +0200

    a

---
+++ RECUPERACIONES Estilos/EVENTOS JS/Hoja 3 UD4 Ordinaria/Ejercicio 3/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const usuario =document.getElementById('usuario').value;
+    const contraseña =document.getElementById('contraseña').value;
+    const edad =document.getElementById('edad').value;
+});
